bede2f5e6fb957515e9a9032d353b1c0ca2b5e61
Mark Helt <heltmm@gmail.com>
2017-08-25 11:17:16 -0700

    add front end logic to convert checked checkboxes into arrays

--- js/scripts.js
+++ js/scripts.js
@@ -2,10 +2,32 @@
 function Cart(name) {
   this.for = name;
   this.pizzas = [];
-}
+};
 function Pizza(size, crust, sauce, topings) {
   this.size = size;
   this.crust = crust;
   this.sauce = sauce;
   this.toppings = topings;
-}
+};
+//front end
+$(document).ready(function() {
+  //takes user name as input and creats a new cart for that name
+  $("#cartInput").submit(function(event){
+    event.preventDefault();
+    var name = $("#nameInput").val();
+    newCart = new Cart(name);
+    console.log(newCart)
+  });
+  $("#pizzaInput").submit(function(event){
+    event.preventDefault();
+    var meatToppings = $("input:checkbox[name=meatTopping]:checked").map(function(){
+      return this.value;
+    }).get();
+    console.log(meatToppings);
+    var nonMeatToppings = $("input:checkbox[name=nonMeatTopping]:checked").map(function(){
+      return this.value;
+    }).get();
+    console.log(nonMeatToppings)
+
+  });
+});
